a1d67f742b2dab3ef0c2288deccaf5c7b7373802
Yureshwar Ravuri <yureshwar@gmail.com>
2019-08-28 10:26:50 +0530

    added the files for the backend functionality

---
+++ src/test/java/voice/backend/indexing/NativeClickeventsIT.java
@@ -0,0 +1,9 @@
+package voice.backend.indexing;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeClickeventsIT extends ClickeventsTest {
+
+    // Execute the same tests but in native mode.
+}
