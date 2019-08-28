1e8f7245ca943f661fceb25cca307d6b15233bf8
Yureshwar Ravuri <yureshwar@gmail.com>
2019-08-28 11:07:59 +0530

    modified the rest application

--- src/main/java/voice/backend/indexing/Clickevents.java
+++ src/main/java/voice/backend/indexing/Clickevents.java
@@ -1,16 +1,23 @@
 package voice.backend.indexing;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/clickeventspush")
+@Path("/clickevents")
 public class Clickevents {
 
     @GET
+    @Path("/all")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "hello";
     }
+
+    @PUT
+    @Path("/push")
+    @Produces(MediaType.TEXT_PLAIN)
+    public
 }
---
+++ src/main/java/voice/backend/indexing/RestApplication.java
@@ -0,0 +1,9 @@
+package voice.backend.indexing;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
