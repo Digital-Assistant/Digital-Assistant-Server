4fd84d6fe263df36f8c49319141fd73003ae578b
Yureshwar Ravuri <yureshwar@gmail.com>
2019-08-29 18:13:06 +0530

    working application of gradle

--- src/test/java/com/nistapp/voice/index/NativeClickeventsIT.java
+++
@@ -1,9 +0,0 @@
-package com.nistapp.voice.index;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeClickeventsIT extends ClickeventsTest {
-
-	// Execute the same tests but in native mode.
-}
