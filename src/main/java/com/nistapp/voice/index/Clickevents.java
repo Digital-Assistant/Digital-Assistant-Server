018786fa5dad109fb8d97d0c3bb612cefda29874
Yureshwar Ravuri <yureshwar@gmail.com>
2019-09-06 16:16:51 +0530

    added two more apis for the application

--- src/main/java/com/nistapp/voice/index/Clickevents.java
+++ src/main/java/com/nistapp/voice/index/Clickevents.java
@@ -1,6 +1,7 @@
 package com.nistapp.voice.index;
 
 import com.nistapp.voice.index.models.JavascriptEvents;
+import org.hibernate.Criteria;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -9,6 +10,7 @@ import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Collection;
+import java.util.List;
 
 @Path("/clickevents")
 public class Clickevents {
@@ -19,9 +21,9 @@ public class Clickevents {
 	@GET
 	@Path("/all")
 	@Produces(MediaType.APPLICATION_JSON)
-	public (Collection<JavascriptEvents>) listalldata() {
-		Query query =  em.createQuery(JavascriptEvents.class);
-		return (Collection<JavascriptEvents>) query.getResultList();
+	public List<JavascriptEvents> listalldata() {
+		Query query = em.createQuery("from JavascriptEvents e",JavascriptEvents.class);
+		return query.getResultList();
 	}
 
 	@POST
@@ -32,6 +34,15 @@ public class Clickevents {
 		em.persist(javascriptevents);
 		em.flush();
 		return javascriptevents;
+	}
+
+	@GET
+	@Path("/fetchbyurl")
+	//@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<JavascriptEvents> listdatabyurl(@QueryParam("url") @DefaultValue("NA") String url) {
+		Query query = em.createQuery("select  e from JavascriptEvents e where e.url=:url",JavascriptEvents.class).setParameter("url",url);
+		return query.getResultList();
 	}
 
 }
