e04c553425ab1b26447568a637daf868a2830b3a
Yureshwar Ravuri <yureshwar@gmail.com>
2020-01-15 22:05:11 +0530

    added the migration script.

---
+++ src/main/resources/db/migration/V1.0.2__clicktrack.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `voice_plugin`.`ClickTrack` (
+	                                         `id` BIGINT NOT NULL AUTO_INCREMENT,
+	                                         `usersessionid` VARCHAR(500) NOT NULL,
+	                                         `clicktype` VARCHAR(200) NOT NULL,
+	                                         `clickedname` VARCHAR(2000) NOT NULL,
+	                                         `recordid` BIGINT NULL,
+	                                         `createdat` BIGINT(20) NULL,
+	                                         PRIMARY KEY (`id`));
