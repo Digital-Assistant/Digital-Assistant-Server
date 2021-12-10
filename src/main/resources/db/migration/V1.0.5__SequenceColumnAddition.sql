8fd2f375e6086692a49a554546434704291e9594
Ajay Kottapally <ajay@sarasanalytics.com>
2021-12-10 19:44:46 +0530

    Additional Params implementation using hibernate search PropertyBridge

--- src/main/resources/db/migration/V1.0.5__SequenceColumnAddition.sql
+++ src/main/resources/db/migration/V1.0.5__SequenceColumnAddition.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `voice_plugin_prod`.`SequenceList` ADD COLUMN `additionalParams` TEXT DEFAULT NULL after `isIgnored`;
+ALTER TABLE `voice_plugin_prod`.`SequenceList` ADD COLUMN `additionalParams` JSON DEFAULT NULL after `isIgnored`;
